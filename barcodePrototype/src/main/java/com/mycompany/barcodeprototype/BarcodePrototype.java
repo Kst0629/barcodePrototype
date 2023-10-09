5a1827aa6b5c28e281d288083db29e49acca833e
VincentNoc <kst0629@autuni.ac.nz>
2023-10-09 14:13:27 +1300

    starting project
    this is the start of the project.

---
+++ barcodePrototype/src/main/java/com/mycompany/barcodeprototype/BarcodePrototype.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.barcodeprototype;
+
+/**
+ *
+ * @author vince-kong
+ */
+public class BarcodePrototype {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
