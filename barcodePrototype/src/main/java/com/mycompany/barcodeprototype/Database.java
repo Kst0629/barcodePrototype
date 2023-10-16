1cdcd8b8d99bfa87bac34de3494d956da806d497
VincentNoc <kst0629@autuni.ac.nz>
2023-10-17 03:16:16 +1300

    aight should work g

--- barcodePrototype/src/main/java/com/mycompany/barcodeprototype/Database.java
+++ barcodePrototype/src/main/java/com/mycompany/barcodeprototype/Database.java
@@ -29,7 +29,7 @@ public class Database {
     private void connectToDatabase() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "AUT4events_!");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "AUT4events_");
             System.out.println("Connected to Database");
 //here sonoo is database name, root is username and password  
             Statement stmt = con.createStatement();
@@ -50,7 +50,7 @@ public class Database {
     private void addNewItem(String barcode, String name){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "AUT4events_!");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "AUT4events_");
             System.out.println("Connected to Database");
 //here sonoo is database name, root is username and password  
             Statement stmt = con.createStatement();
